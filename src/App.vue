138c2efbd1c57e97bd6398a7a2a510bf9a2390a5
marcocalligaris <marcocally@gmail.com>
2022-07-26 19:46:32 +0200

    Imported array with Axios

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
     <BaseHeader />
+    <BaseMain />
   </div>
 </template>
 
 <script>
 import BaseHeader from './components/BaseHeader.vue';
+import BaseMain from './components/BaseMain.vue';
 export default {
   name: 'App',
   components: {
     BaseHeader,
+    BaseMain,
   }
 }
 </script>
--- src/components/AlbumsSection.vue
+++ src/components/AlbumsSection.vue
@@ -1,12 +1,20 @@
 <template>
     <div class="container">
-        
+        <div class="row">
+            
+        </div>
     </div>
 </template>
     
 <script>
+    import axios from 'axios';
     export default {
         name: 'AlbumsSection',
+        mounted(){
+            axios.get('https://flynn.boolean.careers/exercises/api/array/music').then((res) =>{
+                console.log(res.data);
+            });
+        }
     }
 </script>
 
