934b9c445a4124ee77f67becdd2576c9a04b2266
marcocalligaris <marcocally@gmail.com>
2022-07-26 17:19:15 +0200

    Created Base Logo

--- src/components/BaseHeader.vue
+++ src/components/BaseHeader.vue
@@ -1,13 +1,25 @@
 <template>
-    <header>
-        
+    <header class="py-2 px-4">
+        <nav class="d-flex align-items-center">
+            <BaseLogo />
+        </nav>
     </header>
 </template>
 
 <script>
+    import BaseLogo from '../components/BaseLogo.vue';
     export default {
         name: 'BaseHeader',
+        components: {
+            BaseLogo,
+        }
     }
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+@import '../assets/scss/vars';
+header {
+    background-color: $grey;
+    width: 100%;
+}
+</style>
---
+++ src/components/BaseLogo.vue
@@ -0,0 +1,18 @@
+<template>
+        <a href="#">
+            <img src="../assets/img/Spotify_icon.svg.png" alt="Spotify logo">
+        </a>
+</template>
+
+<script>
+    export default {
+        name: 'BaseLogo',
+    }
+</script>
+
+<style lang="scss" scoped>
+    img {
+        width: 50px;
+        height: auto;
+    }
+</style>
