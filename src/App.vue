d9d76e38363103f3d73a45e378bc817cf4c381d1
marcocalligaris <marcocally@gmail.com>
2022-07-26 16:48:31 +0200

    Created Base Header file

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
 <template>
   <div>
-    
+    <BaseHeader />
   </div>
 </template>
 
 <script>
+import BaseHeader from './components/BaseHeader.vue';
 export default {
   name: 'App',
   components: {
-
+    BaseHeader,
   }
 }
 </script>
