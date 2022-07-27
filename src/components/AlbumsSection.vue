e9c3f74b09a1fa3fa5f38abde0d3a681924b0083
marcocalligaris <marcocally@gmail.com>
2022-07-27 09:11:51 +0200

    Created albums carousel

--- src/components/AlbumsSection.vue
+++ src/components/AlbumsSection.vue
@@ -1,25 +1,43 @@
 <template>
     <div class="container">
-        <div class=" albums-list row gy-3">
-            <div class="col-2" v-for="album in albums" :key="album.i">{{album.title}}</div>
+        <div class=" albums-list row py-3 gy-3">
+            <div
+            v-for="album in albums"
+            :key="album.i"
+            class="column"
+            >
+                <AlbumCard
+                :image="album['poster']"
+                :title="album['title']"
+                :author="album['author']"
+                :year="album['year']"
+                />
+            </div>
         </div>
     </div>
 </template>
     
 <script>
     import axios from 'axios';
+    import AlbumCard from './AlbumCard.vue';
     export default {
-        name: 'AlbumsSection',
-        data(){return {
+    name: "AlbumsSection",
+    components: { AlbumCard },
+    data() {
+        return {
             albums: []
-        }},
-        mounted(){
-            axios.get('https://flynn.boolean.careers/exercises/api/array/music').then((res) =>{
-                this.albums = res.data.response;
-            });
-        }
-    }
+        };
+    },
+    mounted() {
+        axios.get("https://flynn.boolean.careers/exercises/api/array/music").then((res) => {
+            this.albums = res.data.response;
+        });
+    },
+}
 </script>
 
 <style lang="scss" scoped>
+    .column {
+        width: 20%;
+    }
 </style>
