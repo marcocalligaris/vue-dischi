dee7b35c74472f0ad5f1c8eedb10d8792324a1ad
marcocalligaris <marcocally@gmail.com>
2022-07-27 09:11:16 +0200

    Album card settings completed

--- src/components/AlbumCard.vue
+++ src/components/AlbumCard.vue
@@ -1,7 +1,33 @@
 <template>
-
+    <div class="album-card p-3 text-center">
+        <img :src="image" alt="title" class="img-fluid mb-3">
+        <h3>{{title}}</h3>
+        <h5>{{author}}</h5>
+        <p>{{year}}</p>
+    </div>
 </template>
 
-<script></script>
-
-<style lang="scss" scoped></style>
+<script>
+export default {
+    name: 'AlbumCard',
+    props: {
+        author: String,
+        image: String,
+        title: String,
+        year: String,
+    }
+}
+</script>
+<style lang="scss" scoped>
+    @import '../assets/scss/vars';
+    .album-card {
+        background-color: $grey;
+        height: 450px;
+        h3 {
+            color: white;
+        }
+        h5, p {
+            color: grey;
+        }
+    }
+</style>
