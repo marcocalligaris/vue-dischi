48a7ebf24dda6b034ba6b22334c54233c256d431
marcocalligaris <marcocally@gmail.com>
2022-07-28 07:21:22 +0200

    Created and added Base Select component

--- src/components/BaseHeader.vue
+++ src/components/BaseHeader.vue
@@ -1,22 +1,27 @@
 <template>
-    <div class="d-flex align-items-center py-2 px-4 ">
+    <nav class=" px-4 d-flex justify-content-between align-items-center">
+        <div class="d-flex align-items-center">
             <BaseLogo />
-    </div>
+        </div>
+        <BaseSelect />
+    </nav>
 </template>
 
 <script>
     import BaseLogo from '../components/BaseLogo.vue';
+    import BaseSelect from '../components/BaseSelect.vue';
     export default {
         name: 'BaseHeader',
         components: {
             BaseLogo,
+            BaseSelect,
         }
     }
 </script>
 
 <style lang="scss" scoped>
 @import '../assets/scss/vars';
-div {
+nav {
     background-color: $grey;
     width: 100%;
     height: 80px;
---
+++ src/components/BaseSelect.vue
@@ -0,0 +1,20 @@
+<template>
+    <select class="form-select form-select-sm">
+        <option selected>Open this select menu</option>
+        <option value="1">One</option>
+        <option value="2">Two</option>
+        <option value="3">Three</option>
+    </select>
+</template>
+
+<script>
+export default {
+    name: 'BaseSelect',
+}
+</script>
+
+<style lang="scss" scoped>
+    select {
+        width: 300px;
+    }
+</style>
